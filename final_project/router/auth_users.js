f82effb7aee8bc07b94ef3118fde349b5e1fb6aa
krobinson96 <kodyrobinson96@gmail.com>
2025-08-11 19:54:20 -0400

    fixed typo in auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -46,7 +46,7 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
 });
 
 // Remove a book review
-regd_users.put("/auth/review/:isbn", (req, res) => {
+regd_users.delete("/auth/review/:isbn", (req, res) => {
     const isbn = req.params.isbn;
     const review = req.query.review;
     const user = jwt.decode(req.session.authorization.accessToken).data
