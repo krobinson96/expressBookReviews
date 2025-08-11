785bd3fd382cc8e7566800559d1df303382c4c93
krobinson96 <kodyrobinson96@gmail.com>
2025-08-11 19:52:13 -0400

    finished tasks

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const axios = require('axios');
 let books = require("./booksdb.js");
 let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const getBooks = async () => {
+    return books;
+}
 
 public_users.post("/register", (req,res) => {
   const {username, password} = req.body.user;
@@ -18,23 +22,27 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-    return res.status(200).send(books);
+public_users.get('/', async function (req, res) {
+    const bookList = await getBooks();
+    return res.status(200).send(JSON.stringify({bookList}, null, 4))
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn', async function (req, res) {
     const isbn = req.params.isbn;
-    return res.status(200).send(JSON.stringify(books[isbn], null, 4));
+    const bookList = await getBooks();
+    if(bookList[isbn]){
+        return res.status(200).send(JSON.stringify(bookList[isbn], null, 4))
+    } else {return res.status(404).json({message:`ISBN ${isbn} not found`})}
 });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
     const author = req.params.author;
     bookList = [];
-    Object.keys(books).forEach((book) => {
-        if(books[book].author == author){
-            bookList.push(books[book]);
+    Object.keys(await getBooks()).forEach(async (book) => {
+        if(await getBooks()[book].author == author){
+            bookList.push(await getBooks()[book]);
         }
     })
     return res.status(200).send(JSON.stringify({bookList}, null, 4))
@@ -44,7 +52,7 @@ public_users.get('/author/:author',function (req, res) {
 public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     let titledBook;
-    Object.keys(books).forEach((book) => {
+    Object.keys(getBooks()).forEach((book) => {
         if(books[book].title == title){
             titledBook = books[book];
         }
